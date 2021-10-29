ffd5c32f23e8d7be4c64cc471c91da11ae4676db
syy11cn <syy11cn@outlook.com>
2021-10-29 17:38:30 +0800

    add sample usage of config router

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { RouterView } from '@syy11cn/config-router';
+import routes from './routes';
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <RouterView
+        routes={routes}
+        onEnter={function (to: string, next: (path?: string) => void): void {
+          // Let '/' and '/404' go.
+          if (to === '/' || to === '/404') {
+            next();
+            return;
+          }
+          // Other routes would be redirected to '/404'
+          next('/404');
+        }}
+      ></RouterView>
     </Router>
   </React.StrictMode>,
   document.getElementById('root'),
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,17 @@
 import { routeType } from '@syy11cn/config-router';
+import App from '@/App';
+import ErrorPage from '@/views/ErrorPage';
 
-const routes: Array<routeType> = [];
+const routes: Array<routeType> = [
+  {
+    path: '/404',
+    component: ErrorPage,
+  },
+  {
+    path: '/',
+    exact: true,
+    component: App,
+  },
+];
 
 export default routes;
